55e9fde1f5a75446bb49d54fd567515a35ebe1d0
Gavin Kimberlin and Hannah Melendy <hannah@>
2019-06-26 16:38:06 -0700

    add css styling.

--- src/sudoku-logic.js
+++ src/sudoku-logic.js
@@ -1,17 +1,3 @@
-export function checkRow (row) {
+export function Row (row) {
   row.forEach(function (array) {
     return (new Set(array)).size !== array.length
-  })
-};
-
-export function checkColumn (column) {
-  row.forEach(function (array) {
-    return (new Set(array)).size !== array.length
-  })
-};
-
-export function checkBox (box) {
-  row.forEach(function (array) {
-    return (new Set(array)).size !== array.length
-  })
-};
