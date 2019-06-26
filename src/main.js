62e569f2a76408d6c67a5e87acda317d9b3ac2eb
Gavin Kimberlin and Hannah Melendy <hannah@>
2019-06-26 15:11:30 -0700

    create a sudoku board where you can input number values from 1 to 9.

---
+++ src/main.js
@@ -0,0 +1,10 @@
+//require('exports-loader?file!./bootstrap/js/dist/.js')
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
+//$(document).ready(function() {
+
+//});
+
+import {checkRow, checkcolumn, checkBox } from './sudoku-logic.js'
---
+++ src/sudoku-logic.js
@@ -0,0 +1,17 @@
+export function checkRow (row) {
+  row.forEach(function (array) {
+    return (new Set(array)).size !== array.length
+  })
+};
+
+export function checkColumn (column) {
+  row.forEach(function (array) {
+    return (new Set(array)).size !== array.length
+  })
+};
+
+export function checkBox (box) {
+  row.forEach(function (array) {
+    return (new Set(array)).size !== array.length
+  })
+};
