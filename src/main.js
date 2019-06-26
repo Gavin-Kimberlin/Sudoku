bacf3494b8e95957cacdec3e52aaceafb698980f
Gavin Kimberlin and Hannah Melendy <gavin@>
2019-06-26 16:36:28 -0700

    link javascript to html.

--- src/main.js
+++ src/main.js
@@ -7,4 +7,27 @@
 
 //});
 
-import {checkRow, checkcolumn, checkBox } from './sudoku-logic.js'
+
+// import { Row } from './sudoku-logic.js'
+// import './styles.css'
+
+
+
+
+
+$(document).ready(function () {
+    $('.userInput').submit(function (event) {
+      event.preventDefault();
+      console.log('working');
+      let col1 = $('.column1').val();
+      let col2 = $('.column2').val();
+      let col3 = $('.column3').val();
+      let col4 = $('.column4').val();
+      let col5 = $('.column5').val();
+      let col6 = $('.column6').val();
+      let col7 = $('.column7').val();
+      let col8 = $('.column8').val();
+      let col9 = $('.column9').val();
+      console.log(col1);
+    });
+});
