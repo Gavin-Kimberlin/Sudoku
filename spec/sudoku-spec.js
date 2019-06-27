408b61dbeb9017f715ab71b2b73bb7ba79fd8529
Gavin Kimberlin and hannah melendy <hannah@>
2019-06-27 11:41:23 -0700

    add specs

---
+++ spec/sudoku-spec.js
@@ -0,0 +1,9 @@
+describe('array', function() {
+
+  it('should only have numbers 1-9', function() {
+    var column1 = new Column(1);
+     column.forEach(function (array) {
+       return (new Set(array)).size !== array.length;
+    });
+  });
+});
