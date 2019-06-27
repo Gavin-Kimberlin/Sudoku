9d1745bb8b7d193bd7e60e4f5843c17cc1dbd4d8
Gavin Kimberlin and hannah melendy <gavin@>
2019-06-27 11:43:14 -0700

    add functionality to input an array of column values

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './css/styles.css';
 //$(document).ready(function() {
-
+function Puzzle () {
+  this.column
+}
 //});
 // col1Arr.forEach(function (col1) {
 //   col1Arr.push(col1);
@@ -18,28 +20,44 @@ $(document).ready(function () {
     event.preventDefault();
     console.log('working');
     var col1Arr = [];
-    $('input.col1').each(function (i) {
+    var array1 = $('input.col1').each(function (i) {
       col1Arr[i] = $(this).val();
-
-      //     $('.userInput').submit(function (event) {
-      //       event.preventDefault();
-      //       console.log('working');
-      //       let col1 = $('.column1').val();
-      //       console.log($('.column1').val());
-      //       let col2 = $('.column2').val();
-      //       let col3 = $('.column3').val();
-      //       let col4 = $('.column4').val();
-      //       let col5 = $('.column5').val();
-      //       let col6 = $('.column6').val();
-      //       let col7 = $('.column7').val();
-      //       let col8 = $('.column8').val();
-      //       let col9 = $('.column9').val();
-      //       console.log(col1);
-
+    });
+    var col2Arr = [];
+    var array2 = $('input.col2').each(function (i) {
+      col2Arr[i] = $(this).val();
+    });
+    var col3Arr = [];
+    var array3 = $('input.col3').each(function (i) {
+      col3Arr[i] = $(this).val();
+    });
+    var col4Arr = [];
+    var array4 = $('input.col4').each(function (i) {
+      col4Arr[i] = $(this).val();
+    });
+    var col5Arr = [];
+    var array5 = $('input.col5').each(function (i) {
+      col5Arr[i] = $(this).val();
+    });
+    var col6Arr = [];
+    var array6 = $('input.col6').each(function (i) {
+      col6Arr[i] = $(this).val();
+    });
+    var col7Arr = [];
+    var array7 = $('input.col7').each(function (i) {
+      col7Arr[i] = $(this).val();
+    });
+    var col8Arr = [];
+    var array8 = $('input.col8').each(function (i) {
+      col8Arr[i] = $(this).val();
+    });
+    var col9Arr = [];
+    var array9 = $('input.col9').each(function (i) {
+      col9Arr[i] = $(this).val();
     });
 
-    // col1Arr.push($('.col1').val());
-    console.log(col1Arr);
+    console.log(col2Arr);
+
 
     // let col2 = $('.column2').val();
     // let col3 = $('.column3').val();
